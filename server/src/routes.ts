bc2110cab34c8eed87608c931f794e3b6a0a2fec
Felipe Komata <fbkomata@gmail.com>
2026-04-24 11:26:21 -0300

    adição de rota para o metodo post

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import {  readAllUsers } from "./controllers/UserController";
+import { createShoes } from "./controllers/CalcadosController";
 
 
 const routes = express.Router();
 
 routes.get("/users", readAllUsers);
 
-
+routes.post("/shoes", createShoes)
 export default routes;
